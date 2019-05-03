5baa27d739f70e631fa5e437227d1fc17a0d69ce
Marcos Gonzales <marcosgonzales@Marcoss-MacBook-Pro.local>
2019-05-03 17:40:08 -0600

    added variables.env

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const path = require('path');
+
+app.set('port', process.env.PORT || 5000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
 
 app.use(express.static(path.join(__dirname, '/public')));
 
