8c438172cdbba6ab0d4156885e3e1f4bc13d719b
Marcos Gonzales <marcosgonzales@Marcoss-MacBook-Pro.local>
2019-05-05 09:22:56 -0600

    fixed the favicon

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = app.listen(app.get('port'), () => {
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')))
 
 app.set('view engine', 'pug');
 
